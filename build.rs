c6dd952bd1bc72c7d12051283c0f338e99a0a22c
ArtemisX64 <101918084+ArtemisX64@users.noreply.github.com>
2024-06-01 19:09:34 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -13,7 +13,7 @@ fn main() {
         ios_platform: { target_os = "ios" },
         windows_platform: { target_os = "windows" },
         apple: { any(target_os = "ios", target_os = "macos") },
-        free_unix: { all(unix, not(apple), not(android_platform), not(target_os = "emscripten")) },
+        free_unix: { all(unix, not(apple), not(target_os = "emscripten")) },
         redox: { target_os = "redox" },
 
         // Native displays.
